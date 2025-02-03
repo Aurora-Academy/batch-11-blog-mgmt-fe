02be58e69879cfd684cb0ca4320414cf2f09664c
Raktim Shrestha <rakimsth@gmail.com>
2025-02-03 19:45:08 +0545

    completed admin layout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AppLayout = () => {
   return <div>AppLayout</div>;
 };
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,5 +1,36 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <a
+            href="/"
+            className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
+          ></a>
+          <span className="mb-3 mb-md-0 text-body-secondary">
+            &copy; 2024 Company, Inc
+          </span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <a className="text-body-secondary" href="#">
+              <svg className="bi" width="24" height="24">
+                {/* <use xlink:href="#twitter" /> */}
+              </svg>
+            </a>
+          </li>
+          <li className="ms-3">
+            <a className="text-body-secondary" href="#">
+              <svg className="bi" width="24" height="24">
+                {/* <use xlink:href="#facebook" /> */}
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
